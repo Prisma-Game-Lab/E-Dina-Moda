d5ae05fd935c716bce8c86d216c73e9ab5806bff
Felipe_Antelo <felipe.antelo.machado@gmail.com>
2023-04-10 20:59:18 -0300

    Implementação do SNAP!
    SNAP! (sim, escreverei assim) está funcionando já para as roupas de corpo, ou como chamadas na cena body. Ainda falta implementar os chapeus e sapatos, mas a lógica é a mesma

--- Projeto-equidnas/Assets/Scripts/DragDrop.cs
+++ Projeto-equidnas/Assets/Scripts/DragDrop.cs
@@ -7,13 +7,17 @@ public class DragDrop : MonoBehaviour, IPointerDownHandler,IBeginDragHandler,IEn
 {
     [SerializeField] private Canvas canvas;
     private RectTransform rectTransform;
+    private CanvasGroup canvasGroup;
     private void Awake()
     {
         rectTransform = GetComponent<RectTransform>();
+        canvasGroup = GetComponent<CanvasGroup>();
     }
     public void OnBeginDrag(PointerEventData eventData)
     {
         Debug.Log("OnBeginDrag");
+        canvasGroup.alpha = .6f;
+        canvasGroup.blocksRaycasts = false;
     }
     public void OnDrag(PointerEventData eventData)
     {
@@ -23,12 +27,15 @@ public class DragDrop : MonoBehaviour, IPointerDownHandler,IBeginDragHandler,IEn
     public void OnEndDrag(PointerEventData eventData)
     {
         Debug.Log("OnEndDrag"); 
+        canvasGroup.alpha = 1f;
+        canvasGroup.blocksRaycasts = true;
     }
     public void OnPointerDown(PointerEventData eventData)
     {
         Debug.Log("OnPointerDown");
 
     }
+    
     void Update()
     {
         
