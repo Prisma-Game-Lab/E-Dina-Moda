a49f30f37dbb94592a747b8418e197502207e5d3
Luiza-Arantes <luiza.acc10@gmail.com>
2023-04-13 22:57:25 -0300

    Som de tirar foto implementado

--- Projeto-equidnas/Assets/Scripts/Screenshot.cs
+++ Projeto-equidnas/Assets/Scripts/Screenshot.cs
@@ -16,6 +16,7 @@ public class Screenshot : MonoBehaviour
     {
         ui.SetActive(false);
         yield return new WaitForSeconds(0.1f);
+        AudioManager.instance.Play("tira_foto");
         ScreenCapture.CaptureScreenshot("Sua_Equidna_" +  System.DateTime.Now.ToString("dd-MM-yy (HH-mm-ss)") + ".png");
         Debug.Log("foto tirada");
         yield return new WaitForSeconds(0.1f);
